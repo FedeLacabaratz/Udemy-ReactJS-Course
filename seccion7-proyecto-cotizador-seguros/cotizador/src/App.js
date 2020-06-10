a71c6c1f90e7ecdbffee3259a9aa110d9fa7fd12
Fede Lacabaratz <lacarq@gmail.com>
2020-06-10 18:27:52 +0200

    add last updates and finished the project

--- seccion7-proyecto-cotizador-seguros/cotizador/src/App.js
+++ seccion7-proyecto-cotizador-seguros/cotizador/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario'
+import Resumen from './components/Resumen'
+import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
 import styled from '@emotion/styled';
 
 const Contenedor = styled.div`
   max-width: 600px;
+  padding: 5rem;
   margin: 0 auto;
 `;
 
@@ -13,13 +17,41 @@ const ContenedorFormulario = styled.div`
   padding: 3rem;
 `;
 function App() {
+
+  const [resumen, setResumen] = useState({
+    cotizacion: 0,
+    marca: '',
+    plan: '',
+    year: ''
+  });
+
+  const [cargando, setCargando] = useState(false);
+
+  const { cotizacion, datos } = resumen;
+
   return (
     <Contenedor>
       <Header
         titulo='Cotizador de Seguros'
       />
       <ContenedorFormulario>
-        <Formulario />
+        <Formulario
+          setResumen={setResumen}
+          setCargando={setCargando}
+        />
+        {datos ? (
+          <Resumen
+            datos={datos}
+          />
+        ) :
+          null
+        }
+        {cargando ? <Spinner /> : null}
+        {!cargando ? 
+        (<Resultado 
+          cotizacion={cotizacion}
+        />) : null}
+        
       </ContenedorFormulario>
     </Contenedor>
   );
