f31b70046d86a123778a76d5730212cada7a7054
unknown <lacarq@gmail.com>
2020-06-17 11:54:25 +0100

    add last updates

--- seccion13-proyecto-buscador-letras-info-de-artistas/letras/src/App.js
+++ seccion13-proyecto-buscador-letras-info-de-artistas/letras/src/App.js
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
+import Formulario from "./components/Formulario";
+import Cancion from "./components/Cancion";
+import Info from "./components/Info";
+import Error from "./components/Error";
 
 function App() {
+  // Definir el state
+  const [searchartlyr, setSearchArtLyr] = useState({});
+  const [lyrics, setLyrics] = useState("");
+  const [info, setInfo] = useState({});
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(searchartlyr).length === 0) return;
+
+    const consultarApiLyrics = async () => {
+      const { artista, cancion } = searchartlyr;
+
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+
+      try {
+        const [letra, informacion] = await Promise.all([
+          axios(url),
+          axios(url2),
+        ]);
+        setLyrics(letra.data.lyrics);
+        setInfo(informacion.data.artists[0]);
+      } catch (err) {
+        setError(true);
+        setTimeout(() => {
+          setError(false);
+        }, 2000);
+        return;
+      }
+    };
+    consultarApiLyrics();
+  }, [searchartlyr]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Formulario setSearchArtLyr={setSearchArtLyr} />
+      {error ? (
+        <Error mensaje="Canción o Grupo no encontrados" />
+      ) : null}
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <Info info={info} />
+          </div>
+          <div className="col-md-6">
+            <Cancion lyrics={lyrics} />
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
