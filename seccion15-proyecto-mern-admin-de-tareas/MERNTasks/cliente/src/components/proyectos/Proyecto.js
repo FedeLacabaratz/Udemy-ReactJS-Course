22f72e91cabd62574bc60712f58421bc111580a1
unknown <lacarq@gmail.com>
2020-06-19 13:38:31 +0100

    add updates and keep on working on this project

--- seccion15-proyecto-mern-admin-de-tareas/MERNTasks/cliente/src/components/proyectos/Proyecto.js
+++ seccion15-proyecto-mern-admin-de-tareas/MERNTasks/cliente/src/components/proyectos/Proyecto.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import ProyectoContext from '../../context/proyectos/ProyectoContext';
+import TareaContext from '../../context/tareas/TareaContext';
 
 const Proyecto = ({proyecto}) => {
 
@@ -7,9 +8,14 @@ const Proyecto = ({proyecto}) => {
     const proyectosContext = useContext(ProyectoContext);
     const { proyectoActual } = proyectosContext;
 
+    // Obtener el state de tareas
+    const tareasContext = useContext(TareaContext);
+    const { obtenerTareas } = tareasContext;
+
     // Maneja el onClick
     const handleOnClick = () => {
-        proyectoActual(proyecto.id);
+        proyectoActual(proyecto.id); // Fijar un proyecto actual
+        obtenerTareas(proyecto.id); // Filtrar las tareas cuando se de click
     }
 
     return ( 
