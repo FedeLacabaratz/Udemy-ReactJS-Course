e6855a644b4ebfafff627d0133697752003da68d
Fede Lacabaratz <lacarq@gmail.com>
2020-06-25 14:14:16 +0200

    add upates and keep on working

--- seccion15-proyecto-mern-admin-de-tareas/MERNTasks/servidor/index.js
+++ seccion15-proyecto-mern-admin-de-tareas/MERNTasks/servidor/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const conectarDB = require('./config/db');
 
 // crear el servidor
 const app = express();
 
+// Conectar a la base de datos
+conectarDB();
+
+// Habilitar express.json
+app.use(express.json({ extended: true }));
+
 // Puerto de la app
 const PORT = process.env.PORT || 4000;
+
+// Importar todas nuestras rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/auth', require('./routes/auth'));
 
 // Arrancar la app
 app.listen(PORT, () => {
---
+++ seccion15-proyecto-mern-admin-de-tareas/MERNTasks/servidor/routes/auth.js
@@ -0,0 +1,17 @@
+// Rutas para autenticar usuarios
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController')
+const { check } = require('express-validator');
+
+// Autentica un usuario
+// Endpoint = api/auth
+router.post('/',
+    [
+        check('email', 'Agrega un email válido').isEmail(),
+        check('password', 'El password debe de ser mínimo de 6 caracteres').isLength({ min: 6 })
+    ],
+    authController.autenticarUsuario
+);
+
+module.exports = router;
