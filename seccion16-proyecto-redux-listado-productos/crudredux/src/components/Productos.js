310b902b77d8d2eee1d0c980ed05670e5c70dbe2
Fede Lacabaratz <lacarq@gmail.com>
2020-07-01 14:52:48 +0200

    add updates and continue working

--- seccion16-proyecto-redux-listado-productos/crudredux/src/components/Productos.js
+++ seccion16-proyecto-redux-listado-productos/crudredux/src/components/Productos.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
+import Producto from './Producto';
+
+// Redux
+import { useSelector, useDispatch } from 'react-redux';
+import { obtenerProductosAction } from '../actions/productosActions';
 
 const Productos = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        // consultar la API
+        const cargarProductos = () => dispatch( obtenerProductosAction() );
+        cargarProductos();
+    }, []);
+
+    // Obtener el state
+    const productos = useSelector(state => state.productos.productos);
+    const error = useSelector(state => state.productos.error);
+    const cargando = useSelector(state => state.productos.loading);
+
     return ( 
-        <h1>Desde Productos.js</h1>
+        <Fragment>
+            <h2 className="text-center my-5">Listado de Productos</h2>
+
+            { error ? <p className="font-weight-bold alert alert-danger text-center mt-4">Hubo un error</p> : null}
+            { cargando ? <p className="text-center mt-4">Cargando...</p> : null}
+
+            <table className="table table-striped">
+                <thead className="bg-primary table-dark">
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { productos.length === 0 ? <p className="mt-3 ">No hay productos</p> : (
+                        productos.map(producto => (
+                            <Producto 
+                                key={producto.id}
+                                producto={producto}
+                            />
+                        ))
+                    )}
+                </tbody>
+            </table>
+        </Fragment>
      );
 }
  
