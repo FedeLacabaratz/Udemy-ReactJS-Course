84ca8851caf45a37063149fec6306bd6bd68b844
Fede Lacabaratz <lacarq@gmail.com>
2020-07-07 13:44:42 +0200

    add updates and keep on working

--- seccion17-proyecto-nextjs-firebase-clon-producthunt/producthuntnext/pages/crear-cuenta.js
+++ seccion17-proyecto-nextjs-firebase-clon-producthunt/producthuntnext/pages/crear-cuenta.js
@@ -1,12 +1,94 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { css } from '@emotion/core';
 import Layout from '../components/layout/Layout';
+import  { Formulario, Campo, InputSubmit, Error } from '../components/ui/Formulario';
 
-const CrearCuenta = () => (
-    <div>
-        <Layout>
-            <h1>Crear Cuenta</h1>
-        </Layout>
-    </div>
-)
+// Validaciones
+
+import useValidacion from '../hooks/useValidacion';
+import validarCrearCuenta from '../validacion/validarCrearCuenta';
+
+const STATE_INICIAL = {
+    nombre: '',
+    email: '',
+    password: ''
+}
+ 
+const CrearCuenta = () => {
+
+    const { valores, errores, submitForm, handleSubmit, handleChange } = useValidacion(STATE_INICIAL, validarCrearCuenta, crearCuenta);
+
+    const { nombre, email, password } = valores;
+
+    function crearCuenta() {
+        console.log('creando cuenta...')
+    }
+
+    return (
+        <div>
+            <Layout>
+                <Fragment>
+                    <h1
+                        css={css`
+                            text-align: center;
+                            margin-top: 5rem;
+                        `}
+                    >Crear Cuenta</h1>
+                    <Formulario
+                        onSubmit={handleSubmit}
+                        noValidate
+                    >
+                        <Campo>
+                            <label htmlFor="nombre">Nombre</label>
+                            <input 
+                                type="text" 
+                                id="nombre"
+                                placeholder="Tu Nombre..."
+                                name="nombre"
+                                value={nombre}
+                                onChange={handleChange}
+                            />
+                        </Campo>
+
+                        {errores.nombre && <Error>{errores.nombre}</Error>}
+
+                        <Campo>
+                            <label htmlFor="email">Email</label>
+                            <input 
+                                type="email" 
+                                id="email"
+                                placeholder="Tu Email..."
+                                name="email"
+                                value={email}
+                                onChange={handleChange}
+                            />
+                        </Campo>
+
+                        {errores.email && <Error>{errores.email}</Error>}
+
+                        <Campo>
+                            <label htmlFor="password">Password</label>
+                            <input 
+                                type="password" 
+                                id="password"
+                                placeholder="Tu Password..."
+                                name="password"
+                                value={password}
+                                onChange={handleChange}
+                            />
+                        </Campo>
+
+                        {errores.password && <Error>{errores.password}</Error>}
+
+                        <InputSubmit
+                            type="submit"
+                            value="Crear Cuenta" 
+                        />
+                    </Formulario>
+                </Fragment>
+            </Layout>
+        </div>
+    )
+}
 
 export default CrearCuenta;
