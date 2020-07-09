2d820840a425a7ba2a2e5fbf2991cf597e340a43
Fede Lacabaratz <lacarq@gmail.com>
2020-07-09 10:24:24 +0200

    add last file

--- seccion17-proyecto-nextjs-firebase-clon-producthunt/producthuntnext/firebase/config.js
+++ seccion17-proyecto-nextjs-firebase-clon-producthunt/producthuntnext/firebase/config.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCAoZol27bBmj-6PAx0KHAFe9VPhaMj9Hs', // Tu apiKey
-  authDomain: "product-hunt-clon.firebaseapp.com", // Tu authDomain
-  databaseURL: "https://product-hunt-clon.firebaseio.com", // Tu databaseURL
-  projectId: "product-hunt-clon", // Tu projectId
-  storageBucket: "product-hunt-clon.appspot.com", // Tu storageBucket
-  messagingSenderId: "346690836952", // Tu messagingSenderId
-  appId: "1:346690836952:web:6c44c7faf0572c9c3ebaad" // Tu AppId
+  apiKey: 'xxxxxxxxxxxxxxxxxxx', // Tu apiKey
+  authDomain: "xxxxxxxxxxxxxxxxxxx", // Tu authDomain
+  databaseURL: "xxxxxxxxxxxxxxxxxxx", // Tu databaseURL
+  projectId: "xxxxxxxxxxxxxxxxxxx", // Tu projectId
+  storageBucket: "xxxxxxxxxxxxxxxxxxx", // Tu storageBucket
+  messagingSenderId: "xxxxxxxxxxxxxxxxxxx", // Tu messagingSenderId
+  appId: "xxxxxxxxxxxxxxxxxxx" // Tu AppId
 };
 
 export default firebaseConfig;
