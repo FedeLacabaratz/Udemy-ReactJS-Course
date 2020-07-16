1cb104bb244a5ff945aeb563a6a9902b6e83f025
Fede Lacabaratz <lacarq@gmail.com>
2020-07-16 15:13:39 +0200

    add updates and start finishing the project

---
+++ seccion19-proyecto-bienesraices-gatsby-graphql-strapi/bienesraicesfrontend/src/components/ListadoPropiedades.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { css } from '@emotion/core';
+import usePropiedades from '../hooks/usePropiedades';
+import PropiedadPreview from './PropiedadPreview';
+import listadoPropiedadesCSS from '../css/listadoPropiedades.module.css';
+import useFiltro from '../hooks/useFiltro';
+
+const ListadoPropiedades = () => {
+
+    const resultado = usePropiedades();
+    const [propiedades] = useState(resultado);
+    const [filtradas, setFiltradas] = useState([]);
+
+    // Filtrado de propiedades
+    const { categoria, FiltroUI } = useFiltro();
+
+    useEffect(() => {
+        if (categoria) {
+            const filtro = propiedades.filter(propiedad => propiedad.categoria.nombre === categoria);
+            setFiltradas(filtro);
+        } else {
+            setFiltradas(propiedades);
+        }
+
+    }, [categoria])
+
+    return (
+        <>
+            <h2
+                css={css`
+                margin-top: 5rem;
+            `}
+            >Nuestras Propiedades</h2>
+            {FiltroUI()}
+            <ul className={listadoPropiedadesCSS.propiedades}>
+                {filtradas.map(propiedad => (
+                    <PropiedadPreview
+                        key={propiedad.id}
+                        propiedad={propiedad}
+                    />
+                ))}
+            </ul>
+        </>
+    );
+}
+
+export default ListadoPropiedades;
