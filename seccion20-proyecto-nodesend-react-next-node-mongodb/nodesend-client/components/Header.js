409ec6f23edfb8daa5f3c609429ae4df97b9773d
Fede Lacabaratz <lacarq@gmail.com>
2020-08-07 14:10:56 +0200

    add updates and continue working on the project

--- seccion20-proyecto-nodesend-react-next-node-mongodb/nodesend-client/components/Header.js
+++ seccion20-proyecto-nodesend-react-next-node-mongodb/nodesend-client/components/Header.js
@@ -1,22 +1,36 @@
 import React, { useContext, useEffect } from 'react';
 import Link from 'next/link';
 import AuthContext from '../context/auth/authContext';
+import AppContext from '../context/app/appContext';
+import { useRouter } from 'next/router'
 
 const Header = () => {
+
+    // Routing
+    const router = useRouter();
 
     // Extraer el usuario autenticado del storage
     const authContext = useContext(AuthContext);
     const { usuario, usuarioAutenticado, cerrarSesion } = authContext;
 
+    // Context de la aplicacion
+    const appContext = useContext(AppContext);
+    const { limpiarState } = appContext;
+
     useEffect(() => {
         usuarioAutenticado()
     }, []);
 
+    const redireccionar = () => {
+        router.push('/');
+        limpiarState();
+    }
+
     return (
         <header className="py-8 flex flex-col md:flex-row items-center justify-between">
-            <Link href="/">
-                <img className="w-64 mb-8 md:mb-0 cursor-pointer" src="logo.svg" />
-            </Link>
+            <img 
+                onClick={() => redireccionar()}
+                className="w-64 mb-8 md:mb-0 cursor-pointer" src="/logo.svg" />
             <div>
                 {
                     usuario ? (
@@ -29,15 +43,15 @@ const Header = () => {
                             >Cerrar Sesión</button>
                         </div>
                     ) : (
-                        <>
-                            <Link href="/login">
-                                <a className="bg-red-500 px-5 py-3 rounded-lg text-white font-bold uppercase mr-2">Iniciar Sesión</a>
-                            </Link>
-                            <Link href="/crearcuenta">
-                                <a className="bg-black px-5 py-3 rounded-lg text-white font-bold uppercase">Crear Cuenta</a>
-                            </Link>
-                        </>
-                    )
+                            <>
+                                <Link href="/login">
+                                    <a className="bg-red-500 px-5 py-3 rounded-lg text-white font-bold uppercase mr-2">Iniciar Sesión</a>
+                                </Link>
+                                <Link href="/crearcuenta">
+                                    <a className="bg-black px-5 py-3 rounded-lg text-white font-bold uppercase">Crear Cuenta</a>
+                                </Link>
+                            </>
+                        )
                 }
             </div>
         </header>
